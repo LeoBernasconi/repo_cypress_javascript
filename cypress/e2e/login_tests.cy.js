3bdc52d2756df1a7e0dfaca98db57ce32bcc88e3
LeoBernasconi <bernasconil@knowfully.com>
2024-08-23 13:36:59 +0200

    concatenate commands on login tests

--- cypress/e2e/login_tests.cy.js
+++ cypress/e2e/login_tests.cy.js
@@ -17,36 +17,30 @@ describe('Login tests', () => {
     })
 
     it('Login OK', () =>{
-        login_page.username_input().clear();
-        login_page.username_input().type(credentials.standar_user);
-        login_page.password_input().clear();
-        login_page.password_input().type(credentials.password);
+        login_page.username_input().clear().type(credentials.standar_user);
+        login_page.password_input().clear().type(credentials.password);
         login_page.login_button().click();
         cy.url().should('include', 'inventory');      
     })
 
     it('Login sending an empty username', () =>{
         login_page.username_input().clear();
-        login_page.password_input().clear();
-        login_page.password_input().type(credentials.password);
+        login_page.password_input().clear().type(credentials.password);
         login_page.login_button().click();
         login_page.error_message().should('be.visible');
         cy.contains("Epic sadface: Username is required").should('be.visible');
     })
 
     it('Login sending an non-registered user', () =>{
-        login_page.username_input().clear();
-        login_page.username_input().type("non_registered_user");
-        login_page.password_input().clear();
-        login_page.password_input().type(credentials.password);
+        login_page.username_input().clear().type("non_registered_user");
+        login_page.password_input().clear().type(credentials.password);
         login_page.login_button().click();
         login_page.error_message().should('be.visible');
         cy.contains("Epic sadface: Username and password do not match any user in this service").should('be.visible');
     })
 
     it('Login sending an empty password', () =>{
-        login_page.username_input().clear();
-        login_page.username_input().type(credentials.standar_user);
+        login_page.username_input().clear().type(credentials.standar_user);
         login_page.password_input().clear();
         login_page.login_button().click();
         login_page.error_message().should('be.visible');
@@ -54,20 +48,16 @@ describe('Login tests', () => {
     })
 
     it('Login sending an incorrect password', () =>{
-        login_page.username_input().clear();
-        login_page.username_input().type(credentials.standar_user);
-        login_page.password_input().clear();
-        login_page.password_input().type("wrong_password");
+        login_page.username_input().clear().type(credentials.standar_user);
+        login_page.password_input().clear().type("wrong_password");
         login_page.login_button().click();
         login_page.error_message().should('be.visible');
         cy.contains("Epic sadface: Username and password do not match any user in this service").should('be.visible');
     })
 
     it('Login with a locked user', () =>{
-        login_page.username_input().clear();
-        login_page.username_input().type(credentials.locked_user);
-        login_page.password_input().clear();
-        login_page.password_input().type(credentials.password);
+        login_page.username_input().clear().type(credentials.locked_user);
+        login_page.password_input().clear().type(credentials.password);
         login_page.login_button().click();
         cy.contains("Epic sadface: Sorry, this user has been locked out.").should('be.visible');
     })

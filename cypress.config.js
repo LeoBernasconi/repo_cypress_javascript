77a52bc2e275325de56e5c6d0f74967484829757
LeoBernasconi <bernasconil@knowfully.com>
2024-08-23 10:52:02 +0200

    initial version of the framework

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+    },
+    specPattern:'**/*cy.js'
+  },
+  
+});
